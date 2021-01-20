4bd0903e40907179310e0c8910ec2a53eb09eab3
miller2590 <59415200+miller2590@users.noreply.github.com>
2021-01-19 20:26:35 -0600

    updated security

--- app.py
+++ app.py
@@ -1,39 +1,44 @@
 from selenium import webdriver
 import time
 
-driver = webdriver.Chrome()
+# Username and password prompt
+username_text = input('Type username: ')
+password_text = input('Type password: ')
+account_number_text = input('Type account number: ')
 
-driver.get('www.brokerage.com')
+# Initiating the driver
+driver = webdriver.Chrome()
+driver.get('www.brokeragesite.com')
 
 # Initial log in table
 username = driver.find_element_by_id('username')
-
-username.send_keys('JohnDoe22')
-
-password = driver.find_element_by_id('password')
-
-password.send_keys('Password1234')
+username.send_keys(username_text)
 
 sign_in = driver.find_element_by_tag_name('button')
-
 sign_in.click()
+
+time.sleep(1.0)
+
+# Next level log in
+password = driver.find_element_by_id('password')
+password.send_keys(password_text)
+
+submit = driver.find_element_by_xpath('/html/body/div/div/div/main/div/div/div[4]/div[1]/form/button')
+submit.click()
 
 time.sleep(3.0)
 
 # Searching for account by entering account number
 account_number = driver.find_element_by_id('account')
-
-account_number.send_keys('555-555-555')
+account_number.send_keys(account_number_text)
 
 search = driver.find_element_by_xpath('//*[@id="app-menu"]/form/div[2]/button')
-
 search.click()
 
 time.sleep(1.0)
 
 # Loading overview of positions
 positions = driver.find_element_by_xpath("/html/body/div[2]/div/div/section/div[2]/div[5]/div/div/header/span[2]/a/div")
-
 positions.click()
 
 time.sleep(1.0)
@@ -41,7 +46,6 @@ time.sleep(1.0)
 # Finally, downloading csv of positions and closing browser
 download_file = driver.find_element_by_xpath('/html/body/div[2]/div/div/section/div[2]/div['
                                              '2]/div/ui-grid-export-tools/div/export-ui-grid-csv/button')
-
 download_file.click()
 
 time.sleep(2.0)
