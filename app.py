f0af704f3ae635e83f20f6ac118080a6aadee3f6
miller2590 <59415200+miller2590@users.noreply.github.com>
2021-01-20 18:09:54 -0600

    Replaced time.sleep with WebDriverWait()

--- app.py
+++ app.py
@@ -1,51 +1,53 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 # Username and password prompt
-username_text = input('Type username: ')
-password_text = input('Type password: ')
-account_number_text = input('Type account number: ')
+username_text = input('Enter username: ')
+password_text = input('Enter password: ')
+account_number_text = input('Enter account number: ')
 
 # Initiating the driver
 driver = webdriver.Chrome()
-driver.get('www.brokeragesite.com')
+driver.get('www.randombrokeragewebsite.com')
 
 # Initial log in table
-username = driver.find_element_by_id('username')
+username = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'username')))
 username.send_keys(username_text)
 
 sign_in = driver.find_element_by_tag_name('button')
 sign_in.click()
 
-time.sleep(1.0)
-
 # Next level log in
-password = driver.find_element_by_id('password')
+password = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'password')))
 password.send_keys(password_text)
 
 submit = driver.find_element_by_xpath('/html/body/div/div/div/main/div/div/div[4]/div[1]/form/button')
 submit.click()
 
-time.sleep(3.0)
-
 # Searching for account by entering account number
-account_number = driver.find_element_by_id('account')
+account_number = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'account')))
 account_number.send_keys(account_number_text)
 
 search = driver.find_element_by_xpath('//*[@id="app-menu"]/form/div[2]/button')
 search.click()
 
-time.sleep(1.0)
-
 # Loading overview of positions
-positions = driver.find_element_by_xpath("/html/body/div[2]/div/div/section/div[2]/div[5]/div/div/header/span[2]/a/div")
+positions = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div["
+                                                                                      "2]/div/div/section/div[2]/div["
+                                                                                      "5]/div/div/header/span["
+                                                                                      "2]/a/div")))
 positions.click()
 
-time.sleep(1.0)
-
 # Finally, downloading csv of positions and closing browser
-download_file = driver.find_element_by_xpath('/html/body/div[2]/div/div/section/div[2]/div['
-                                             '2]/div/ui-grid-export-tools/div/export-ui-grid-csv/button')
+download_file = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div['
+                                                                                          '2]/div/div/section/div['
+                                                                                          '2]/div[ '
+                                                                                          '2]/div/ui-grid-export'
+                                                                                          '-tools/div/export-ui-grid'
+                                                                                          '-csv/button')))
 download_file.click()
 
 time.sleep(2.0)
