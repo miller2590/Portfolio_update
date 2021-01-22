51fea81ddbb7c629ff256ef99f5d1005c14f3a37
miller2590 <59415200+miller2590@users.noreply.github.com>
2021-01-22 09:48:25 -0600

    Finished product, Still need to get rid of duplicate company names in simply safe csv file.

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
+import os
+import time
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 
 # Username and password prompt
-username_text = input('Enter username: ')
-password_text = input('Enter password: ')
-account_number_text = input('Enter account number: ')
+username_text = input('Enter Apex username: ')
+password_text = input('Enter Apex password: ')
+account_number_text = input('Enter Apex account number: ')
+simply_safe_email_text = input('Enter Simply Safe Email: ')
+simply_safe_password_text = input('Enter Simply Safe password: ')
 
 # Initiating the driver
 driver = webdriver.Chrome()
-driver.get('www.randombrokeragewebsite.com')
+driver.get('https://public-apps.apexclearing.com')
 
 # Initial log in table
 username = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'username')))
@@ -51,5 +55,79 @@ download_file = WebDriverWait(driver, 10).until(EC.presence_of_element_located((
 download_file.click()
 
 time.sleep(2.0)
+
+driver.close()
+
+# Parsing csv data
+df = pd.read_csv('/home/gage/Downloads/positions-export.csv')
+
+ticker_price_amount = df[['Symbol', 'Qty', 'Price']]
+
+ticker_price_amount.to_csv('SimplySafeData.csv', index=False)
+
+os.remove('/home/gage/Downloads/positions-export.csv')
+
+# Returning data to Simply Safe website
+driver = webdriver.Chrome()
+driver.get('https://www.simplysafedividends.com/')
+
+# Simply Safe log in
+log_in = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/nav/ul[1]/li[5]/a')))
+log_in.click()
+
+# Simply safe email
+simply_safe_email = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'login')))
+simply_safe_email.send_keys(simply_safe_email_text)
+
+enter_log_in = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div/form/div'
+                                                                                         '/input')))
+enter_log_in.click()
+
+# Simply safe password
+simply_safe_password = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'password')))
+simply_safe_password.send_keys(simply_safe_password_text)
+
+enter_password = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div/form'
+                                                                                           '/div/input')))
+enter_password.click()
+
+# Navigate to portfolio update source
+portfolio_menu = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="global-nav'
+                                                                                           '"]/div[1]/a')))
+portfolio_menu.click()
+
+# Select portfolio
+my_portfolio = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="global-nav"]/div['
+                                                                                         '1]/div/div/nav/section['
+                                                                                         '1]/a[1]')))
+my_portfolio.click()
+
+# Selecting import data
+import_menu = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="investments'
+                                                                                        '"]/header/div[1]/div/a')))
+import_menu.click()
+
+spreadsheet_selection = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*['
+                                                                                                  '@id="investments'
+                                                                                                  '"]/header/div['
+                                                                                                  '1]/div/div/a[1]')))
+spreadsheet_selection.click()
+
+# Choose account
+investment_account = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div'
+                                                                                               '/article/div/a[2]')))
+investment_account.click()
+
+# Navigate to file options
+file_choice = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="spreadsheet'
+                                                                                        '_import_file"]')))
+file_choice.send_keys('/home/gage/PycharmProjects/Portfolio update/SimplySafeData.csv')
+
+# Finally, uploading the document and close driver
+upload_csv = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div/article/div['
+                                                                                       '3]/a[1]')))
+upload_csv.click()
+
+os.remove('/home/gage/PycharmProjects/Portfolio update/SimplySafeData.csv')
 
 driver.close()
